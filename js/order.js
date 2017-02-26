2e8ed01e56dcd0834e80370a4cbad510b887be46
findawayer <findawayer@gmail.com>
2017-02-26 18:37:38 +0900

    order: Refine select box control

--- js/order.js
+++ js/order.js
@@ -11,14 +11,6 @@
 	tq.plusButton.addEventListener( "click", increaseQuantity );
 	tq.minusButton.addEventListener( "click", decreaseQuantity );
 
-	// 메일 계정을 <select/> 로 선택할 수 있게
-	var ea = {
-		field: document.getElementById( "userEmail2" ),
-		select: document.getElementById( "selectEmail" )
-	}
-
-	ea.select.addEventListener( "change", mailAccountControl );
-
 	function increaseQuantity() {
 		var val = Number( tq.field.value );
 
@@ -37,15 +29,34 @@
 		}
 	}
 
-	function mailAccountControl() {
+	// 메일 계정을 <select/> 로 선택할 수 있게
+	var ea = {
+		field: document.getElementById( "userEmail2" ),
+		select: document.getElementById( "selectEmail" )
+	}
+
+	ea.select.addEventListener( "change", mailAccountSelect );
+	ea.field.addEventListener( "focus", mailAccountCustomInput );
+
+	// 셀렉트 박스를 선택할 때
+	function mailAccountSelect() {
 		var val = ea.select.options[ea.select.selectedIndex].value;
 
+		// 프리셋을 선택하면 그 값을 input에 반영
 		if ( val != "custom" ) {
 			ea.field.value = val;
-		} else {
+		}
+		// `직접입력`을 선택하면 input을 비우고 포커스
+		else {
 			ea.field.value = "";
 			ea.field.focus();
 		}
+	}
+
+	// 직접입력할 때
+	function mailAccountCustomInput() {
+		// 셀렉트 박스를 `직접입력`으로 선택
+		ea.select.selectedIndex = 0;
 	}
 
 })( document );
