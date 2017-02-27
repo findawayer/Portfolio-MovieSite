e7a657606e4b69d622b7781769138f389abe0f93
findawayer <findawayer@gmail.com>
2017-02-27 17:56:21 +0900

    Apply jQuery and smoothTouchScroll plugin scripts

--- js/index.js
+++ js/index.js
@@ -1,46 +1,7 @@
-;( function( document ){
+$(function() {
 
-	window.addEventListener( "DOMContentLoaded", function() {
+	// 영화목록을 터치 드래깅 가능하도록
+	// smoothTouchScroll, jQuery, jQuery UI, jQuery kinetic 사용
+	$("#ongoingMoviesGallery, #scheduledMoivesGallery").smoothTouchScroll();
 
-		/* 영화목록 masonry */
-		var movieGrid = document.getElementById( "movieGrid" );
-		var movieFilter = document.getElementById( "movieFilter" );
-
-		if ( movieGrid || movieFilter )  {
-			var movieGridIso = new Isotope( movieGrid, {
-				itemSelector: ".gallery__item",
-				layoutMode: 'fitRows',
-				masonry: {
-					columnWidth: 220
-				}
-			});
-
-			var filterFn = function( itemEl ) {
-				var filter = new RegExp( event.target.innerText );
-				var movieItemGenre = itemEl.querySelector( ".gallery__item__genre" ).innerText;
-
-				return filter.test( movieItemGenre );
-			};
-
-			movieFilter.addEventListener( "click", function( event ) {
-				var target = event.target;
-
-				if ( target.tagName == "BUTTON" ) {
-					target.parentElement.querySelector( ".selected" ).classList.remove( "selected" );
-					target.classList.add( "selected" );
-
-					var filterValue = event.target.hasAttribute( "data-filter" )
-						? event.target.getAttribute( "data-filter" )
-						: filterFn;
-
-					movieGridIso.arrange({
-						filter: filterValue
-					});
-
-				}
-			});
-		}
-
-	});
-
-})( document );
+});
--- js/order.js
+++ js/order.js
@@ -1,72 +1,71 @@
-;( function( document ) {
+;( function( window, document, $, undefined ) {
 
 	// 티켓 매수 (Ticket Quantity) 조정
 	var tq = {
-		field: document.getElementById( "orderQuantity" ),
-		plusButton: document.getElementById( "increaseQuantity" ),
-		minusButton: document.getElementById( "decreaseQuantity" ),
+		$field: $( "#orderQuantity" ),
+		$plusButton: $( "#increaseQuantity" ),
+		$minusButton: $( "#decreaseQuantity" ),
 		maxValue: 10
 	}
 
-	tq.plusButton.addEventListener( "click", function( event ) {
+	tq.$plusButton.on( "click", function( event ) {
 		event.preventDefault();
 		increaseQuantity();
 	});
-	tq.minusButton.addEventListener( "click", function( event ) {
+	tq.$minusButton.on( "click", function( event ) {
 		event.preventDefault();
 		decreaseQuantity();
 	});
 
 	function increaseQuantity() {
-		var val = Number( tq.field.value );
+		var currentValue = Number( tq.$field.val() );
 
-		if ( val < tq.maxValue ) {
-			tq.field.value = val + 1;
+		if ( currentValue < tq.maxValue ) {
+			tq.$field.val( currentValue + 1 );
 		} else {
 			alert( "티켓은 한 번에 " + tq.maxValue + "장까지 예매하실 수 없습니다." );
 		}
-
-		return false;
 	}
 
 	function decreaseQuantity() {
-		var val = Number( tq.field.value );
+		var currentValue = Number( tq.$field.val() );
 
-		if ( val > 1 ) {
-			tq.field.value = val - 1;
+		if ( currentValue > 1 ) {
+			tq.$field.val( currentValue - 1 );
 		}
-
-		return false;
 	}
 
 	// 메일 계정을 <select/> 로 선택할 수 있게
 	var ea = {
-		field: document.getElementById( "userEmail2" ),
-		select: document.getElementById( "selectEmail" )
+		$field: $( "#userEmail2" ),
+		$select: $( "#selectEmail" )
 	}
 
-	ea.select.addEventListener( "change", mailAccountSelect );
-	ea.field.addEventListener( "focus", mailAccountCustomInput );
+	ea.$select.on( "change", function() {
+		mailAccountSelect();
+	});
+	ea.$field.on( "focus", function() {
+		mailAccountCustomInput();
+	});
 
 	// 셀렉트 박스를 선택할 때
 	function mailAccountSelect() {
-		var val = ea.select.options[ea.select.selectedIndex].value;
+		var selectedValue = ea.$select.val();
 
 		// 프리셋을 선택하면 그 값을 input에 반영
-		if ( val != "custom" ) {
-			ea.field.value = val;
+		if ( selectedValue != "custom" ) {
+			ea.$field.val( selectedValue );
 		}
 		// `직접입력`을 선택하면 input을 비우고 포커스
 		else {
-			ea.field.value = "";
-			ea.field.focus();
+			ea.$field.val( "" ).focus();
 		}
 	}
 
 	// 직접입력할 때
 	function mailAccountCustomInput() {
 		// 셀렉트 박스를 `직접입력`으로 선택
-		ea.select.selectedIndex = 0;
+		ea.$select.val( "custom" );
 	}
 
-})( document );
+})( window, document, jQuery );
