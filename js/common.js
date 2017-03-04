01dcec02e647d3f212dd6cf458204528f3940de1
findawayer <findawayer@gmail.com>
2017-03-05 06:12:29 +0900

    Merge smoothTouchScroll action to common.js

--- js/common.js
+++ js/common.js
@@ -1,24 +1,26 @@
-!function( window, document, $, undefined ){
+;( function( window, document, $, undefined ){
 
 	/* 슬라이드아웃 메뉴 활성화 */
-	!function slideMenuToggle() {
+	( function slideMenuToggle() {
 		var $hamburger = $( "#sidemenuTrigger" );
 		var $sidemenu = $( "#sidemenu" );
 
-		$hamburger.on( "click", function(event) {
+		$hamburger.on( "click", function( event ) {
 			event.preventDefault();
 			$sidemenu.toggleClass( "open" );
 		});
-	}();
+	})();
 
-	/* 플로팅 인풋 플레이스홀더 */
-	!function floatingLabels( $targets ) {
-		if ( !$targets.length > 0 ) return;
+	/* 플로팅 레이블 */
+	( function floatingLabels( $targets ) {
+		if ( !$targets.length ) return;
 
+		// 모든 플로팅 레이블에 이벤트 리스너 추가
 		$targets.each( function( i, el ) {
 			setFloatingLabel( el );
 		});
 
+		// 플로팅 레이블 준비
 		function setFloatingLabel( trigger ) {
 			var $trigger = $( trigger );
 			var $field = $trigger.parent().find( ".ui-input" );
@@ -36,30 +38,34 @@
 				}
 			}
 		}
-	}( $( ".ui-input-placeholder" ) );
+	})( $( ".ui-input-placeholder" ) );
 
 
 	/* 더 읽기 */
-	!function collapsibleText( $targets ) {
-		if ( !$targets.length > 0 ) return;
+	( function collapsibleText( $targets ) {
+		if ( !$targets.length ) return;
 
+		// 모든 '더 읽기' 버튼에 이벤트 리스너 추가
 		$targets.each( function( i, el ) {
 			initEvent( el );
 		});
 
+		// 클릭시 감춰진 텍스트 펼치기
 		function initEvent( textBlock ) {
 			var $textBlock = $( textBlock );
 
 			$textBlock.find( ".read-more" ).on( "click", function( event ) {
 				event.preventDefault();
-				expandText( $textBlock );
+				$textBlock.addClass( "is-expanded" );
 			});
 		}
+	})( $( ".text-collapsed" ) );
 
-		function expandText( textBlock ) {
-			textBlock.addClass( "is-expanded" );
-		}
+	/* 터치 횡스크롤 활성화
+	 * 플러그인: smoothTouchScroll
+	 * 디펜던시: jQuery, jQuery UI, jQuery kinetic */
+	if ( $.fn.smoothTouchScroll ) {
+		$(".touch-scrollable").smoothTouchScroll();
+	}
 
-	}( $( ".text-collapsed" ) );
-
-}( window, document, jQuery );
+})( window, document, jQuery );
--- js/index.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-
-	// 영화목록을 터치 드래깅 가능하도록
-	// smoothTouchScroll, jQuery, jQuery UI, jQuery kinetic 사용
-	$("#ongoingMoviesGallery, #scheduledMoivesGallery").smoothTouchScroll();
-
-});
